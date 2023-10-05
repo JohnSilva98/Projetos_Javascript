9e73afef44fc6ab25c1154529e38c5164573bf6f
JohnSilva98 <jonathan.design16@oulook.com>
2023-10-05 15:52:48 -0300

    string

--- Operadores/js/scripts.js
+++ Operadores/js/scripts.js
@@ -1,3 +1,20 @@
+//number
 console.log(typeof 2)
 console.log(typeof 5.3)
 console.log(typeof -127)
+
+//Ops. aritmeticas
+console.log(2 + 4)
+console.log(10 - 5)
+console.log(3 * 2)
+console.log(12 / 4)
+
+//special numbers
+console.log(typeof Infinity)
+console.log(typeof -Infinity)
+console.log(12 * "asd")
+console.log(typeof NaN)
+
+//string
+console.log('um texto')
+console.log('outro texto')
