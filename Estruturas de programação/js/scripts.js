20c917159292e24a0569f0a464780499b0ff4018
JohnSilva98 <jonathan.design16@oulook.com>
2023-10-06 11:31:37 -0300

    alert e math

--- Estruturas de programação/js/scripts.js
+++ Estruturas de programação/js/scripts.js
@@ -2,5 +2,34 @@
 let nome = "John"
 console.log(nome)
 
-const idade = 31;
+const idade = 31
 //const não tem seu valor alterado
+
+//nomes inválidos de variaveis
+// let 2teste = "invalido"
+// let @teste = "invalido"
+
+let a = 10,
+  b = 20,
+  c = 30
+
+console.log(a, b, c)
+//nomes permitidos de variaveis
+let _teste = "ok"
+let $teste = "Ok"
+
+//prompt
+
+//const age = prompt("Digite sua idade: ")
+//console.log("você tem " + age + " anos")
+
+//alert
+
+//alert("Clica para fechar")
+
+//math
+
+console.log(Math.max(5, 2, 5, 11, 64, 2))
+
+console.log(Math.floor(5.13))
+//console
