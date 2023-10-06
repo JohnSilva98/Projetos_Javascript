a5eba2e26dd2887f36d4f0d6fdc81af71acf51db
JohnSilva98 <jonathan.design16@oulook.com>
2023-10-06 11:49:03 -0300

    estrutura de decisão

--- Estruturas de programação/js/scripts.js
+++ Estruturas de programação/js/scripts.js
@@ -33,3 +33,16 @@ console.log(Math.max(5, 2, 5, 11, 64, 2))
 
 console.log(Math.floor(5.13))
 //console
+
+console.log("log exibe mensagens na console")
+console.error("erro!")
+console.warn("warm da um aviso na console")
+
+//estruturas de controle
+
+const age = prompt("digite sua idade; ")
+if (age >= 18) {
+  console.log("Você tem 18 anos ou mais!")
+} else {
+  console.log("Você tem menos de 18 anos!")
+}
