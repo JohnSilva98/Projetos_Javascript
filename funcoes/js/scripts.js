f4f9817f60eabcd8e9a2506f92319c43b4bfdad3
JohnSilva98 <jonathan.design16@oulook.com>
2023-10-10 09:24:18 -0300

    arrow function

--- funcoes/js/scripts.js
+++ funcoes/js/scripts.js
@@ -5,3 +5,33 @@ function soma(a, b) {
 }
 
 console.log(soma(1, 3))
+
+//escopo de função
+
+let y = 10
+
+function testEscopo() {
+  let y = 24
+  console.log(`escopo dentro da função é:  ${y}`)
+}
+testEscopo()
+
+console.log(`y fora da função é: ${y} `)
+
+//arrow function
+
+const parOuImpar = (n) => {
+  if (n % 2 === 0) {
+    console.log("Número par")
+    return
+  }
+  console.log("Impar")
+}
+
+parOuImpar(1)
+parOuImpar(4)
+
+//arrow function resumida
+
+const raizQuadrada = (x) => x * x
+console.log(raizQuadrada(4))
