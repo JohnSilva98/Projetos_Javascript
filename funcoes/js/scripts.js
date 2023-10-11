f6961e2b88cf35ea9a33455ac7c87270a72efd11
JohnSilva98 <jonathan.design16@oulook.com>
2023-10-11 10:15:23 -0300

    seção finalizada

--- funcoes/js/scripts.js
+++ funcoes/js/scripts.js
@@ -35,3 +35,27 @@ parOuImpar(4)
 
 const raizQuadrada = (x) => x * x
 console.log(raizQuadrada(4))
+
+//recursividade
+
+const untilTen = (n, m) => {
+  if (n < 10) {
+    console.log("a função parou de executar")
+  } else {
+    const x = n - m
+    console.log(x)
+    untilTen(x, m)
+  }
+}
+untilTen(100, 7)
+
+function factorial(x) {
+  if (x === 0) {
+    return 1
+  } else {
+    return x * factorial(x - 1)
+  }
+}
+const num = 6
+const result = factorial(num)
+console.log(result)
