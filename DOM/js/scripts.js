66205856636048f8405c1e73eb3ebeee96d73054
JohnSilva98 <jonathan.design16@oulook.com>
2023-10-13 10:46:38 -0300

    selecionando elementos

---
+++ DOM/js/scripts.js
@@ -0,0 +1,13 @@
+console.log(document.body)
+
+const listItems = document.getElementsByTagName("li")
+console.log(listItems)
+
+const title = document.getElementById("title")
+console.log(title)
+
+const classe = document.getElementsByClassName("product")
+console.log(classe)
+
+const productsQuery = document.querySelectorAll(".product")
+console.log(productsQuery)
